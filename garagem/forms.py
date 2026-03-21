87c7350f3e0c5201f8da78bc1ac9b97dabc2a358
Gabrieldanttasxl <felipegabrieldsd@gmail.com>
2026-03-20 21:24:37 -0300

    style: melhora interface com Bootstrap e formularios

--- garagem/forms.py
+++ garagem/forms.py
@@ -6,6 +6,10 @@ class GaragemForm(forms.ModelForm):
     class Meta:
         model = Garagem
         fields = ['nome', 'descricao']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'descricao': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+        }
 
 
 class VeiculoForm(forms.ModelForm):
@@ -21,3 +25,13 @@ class VeiculoForm(forms.ModelForm):
             'tipo_veiculo',
             'observacao',
         ]
+        widgets = {
+            'garagem': forms.Select(attrs={'class': 'form-select'}),
+            'vin': forms.TextInput(attrs={'class': 'form-control'}),
+            'marca': forms.TextInput(attrs={'class': 'form-control'}),
+            'modelo': forms.TextInput(attrs={'class': 'form-control'}),
+            'ano_modelo': forms.TextInput(attrs={'class': 'form-control'}),
+            'fabricante': forms.TextInput(attrs={'class': 'form-control'}),
+            'tipo_veiculo': forms.TextInput(attrs={'class': 'form-control'}),
+            'observacao': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+        }
